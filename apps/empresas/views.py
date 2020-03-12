b0d736b9dadbb89913291cbec4dd232f305bbd4d
Lucas Fernandes <lucashfernandes@yahoo.com.br>
2020-03-12 19:12:13 -0300

    Adicionado templates em funcinario

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Empresa
 
-# Create your views here.
 
 class EmpresaCreate(CreateView):
     model = Empresa
     fields = ['nome']
+
 
     def form_valid(self, form):
         obj = form.save()
@@ -15,6 +15,7 @@ class EmpresaCreate(CreateView):
         funcionario.empresa = obj
         funcionario.save()
         return HttpResponse('Ok')
+
 
 class EmpresaEdit(UpdateView):
     model = Empresa
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView
 from .models import Funcionario
 
 
-def FuncionariosList(ListView):
+class FuncionariosList(ListView):
     model = Funcionario
-
 
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
-        return Funcionario.ojects.filter(empresa=empresa_logada)
+        return Funcionario.objects.filter(empresa=empresa_logada)
 
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('list_funcionarios')
